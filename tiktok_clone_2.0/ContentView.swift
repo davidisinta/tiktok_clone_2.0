c5d95257350645e3ebda6a539c562dac467a94fd
David Isinta <david.isinta@gmail.com>
2024-02-24 00:49:34 -0500

    Initial Commit

---
+++ tiktok_clone_2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tiktok_clone_2.0
+//
+//  Created by David Isinta Nyakawa on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tiktok_clone_2.0/tiktok_clone_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  tiktok_clone_2_0App.swift
+//  tiktok_clone_2.0
+//
+//  Created by David Isinta Nyakawa on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct tiktok_clone_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
